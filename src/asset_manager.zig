6d75d3c4573df7491927af7c49dcccf615e50c1e
cespeute <espeut.clement@gmail.com>
2022-06-07 23:30:43 +0200

    Mini rect lib

--- src/asset_manager.zig
+++ src/asset_manager.zig
@@ -31,16 +31,136 @@ pub const PackBuilder = struct {
 };
 
 const Rgba = packed struct {
-    r : u8,
-    g : u8,
-    b : u8,
-    a : u8,
+    r : u8 = 0,
+    g : u8 = 0,
+    b : u8 = 0,
+    a : u8 = 0,
 };
 
 inline fn indexBitmap(bitmap : []const Rgba, width : isize, x : isize, y : isize) Rgba
 {
     return bitmap[@intCast(usize, x + y * width)];
 }
+
+const Rect = struct {
+    x : i16 = 0,
+    y : i16 = 0,
+    w : i16 = 0,
+    h : i16 = 0,
+
+    pub fn initFromPoints(x0 : i16, y0 : i16, x1 : i16, y1 : i16) Rect
+    {
+        return .{
+            .x = x0,
+            .y = y0,
+            .w = @maximum(x1 - x0, 0),
+            .h = @maximum(y1 - y0, 0),
+        };
+    }
+
+    pub fn intersection(a : Rect, b : Rect) Rect
+    {
+        const ax1 = a.x + a.w;
+        const ay1 = a.y + a.h;
+
+        const bx1 = b.x + b.w;
+        const by1 = b.y + b.h;
+
+        return initFromPoints(
+            @maximum(a.x, b.x),
+            @maximum(a.y, b.y),
+            @minimum(ax1, bx1),
+            @minimum(ay1, by1),
+        );
+    }
+};
+
+pub const Bitmap = struct {
+    data : []Rgba = undefined,
+    width : u16 = undefined,
+    height : u16 = undefined,
+
+    // Creates a bitmap with allocated random memory
+    pub fn init(in_width : u16, in_height : u16, allocator:Allocator) !Bitmap
+    {
+        return Bitmap {
+            .data = try allocator.alloc(Rgba, @intCast(usize, in_width) * @intCast(usize, in_height)),
+            .width = in_width,
+            .height = in_height,
+        };
+    }
+
+    // Take ownership of the in_data pointer
+    pub fn initFromU8(in_data : []u8, in_width : u16, in_height : u16) Bitmap
+    {
+        return Bitmap{
+            .data = std.mem.bytesAsSlice(Rgba, in_data),
+            .width = in_width,
+            .height = in_height,
+        };
+    }
+
+    pub fn deinit(self : *Bitmap, allocator : Allocator) void
+    {
+        allocator.free(self.data);
+        self.* = undefined;
+    }
+
+    pub fn clear(self : *const Bitmap) void
+    {
+        std.mem.set(Rgba, self.data, Rgba{});
+    }
+
+    pub fn getPixel(self : *const Bitmap, x : u16, y : u16) !Rgba
+    {
+        if (x >= self.width or y >= self.height) return error.OutOfBounds;
+        return self.getPixelUnchecked(x, y);
+    }
+
+    pub inline fn getPixelUnchecked(self : *const Bitmap, x : u16, y : u16) Rgba
+    {
+        return self.idx(x,y).*;
+    }
+
+    pub fn setPixelUnchecked(self : *const Bitmap, x : u16, y : u16, pixel : Rgba) void
+    {
+        self.idx(x,y).* = pixel;
+    }
+
+    inline fn idx(self : *const Bitmap, x : u16, y : u16) *Rgba
+    {
+        const index : usize = @intCast(usize, x) + @intCast(usize, y * self.width); 
+        return &self.data[index];
+    }
+
+    pub fn debugPrint(self : *const Bitmap) void
+    {
+        std.debug.print("\n", .{});
+
+        var y : isize = 0;
+        while (y < self.height)
+        {
+            var x : isize = 0;
+            while (x < self.width)
+            {
+                var char = if (self.getPixelUnchecked(@intCast(u16, x), @intCast(u16, y)).a == 0) "." else "#";
+                std.debug.print("{s}", .{char});
+                x += 1;
+            }
+            std.debug.print("\n", .{});
+            y += 1;
+        }
+    }
+
+    fn blit(self : *const Bitmap, source : *const Bitmap, source_rect : Rect, dest_rect : Rect) void
+    {
+        _ = self;
+        _ = source;
+        _ = source_rect;
+        _ = dest_rect;
+    }
+
+};
 
 pub const PngPacker = struct {
     allocator : Allocator = undefined,
@@ -154,6 +274,15 @@ pub const PngPacker = struct {
     }
 };
 
+test "rect intersection" {
+    const a = Rect.initFromPoints(15, 20, 42, 69);
+    const b = Rect.initFromPoints(30, 15, 67, 65);
+    const inter = Rect.intersection(a, b);
+
+    try std.testing.expectEqual(Rect.initFromPoints(30, 20, 42, 65), inter);
+    
+}
+
 
 test "build"
 {
@@ -185,7 +314,18 @@ test "build"
 
     try std.testing.expect(indexBitmap(ret.bitmap, @intCast(isize, ret.width), 0, 0).a == 0);
     try std.testing.expect(indexBitmap(ret.bitmap, @intCast(isize,ret.width), 1, 0).a != 0);
+}
 
+test "bitmap"
+{
+    var bitmap =  try Bitmap.init(16,16, std.testing.allocator);
+    defer bitmap.deinit(std.testing.allocator);
+
+    bitmap.clear();
+
+    bitmap.setPixelUnchecked(0, 0, .{.a = 255});
+
+    bitmap.debugPrint();
 }
 
 pub fn main() !void
